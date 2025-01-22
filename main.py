acbb0a7bb7ca2d59cf23063246cd277ebc1f8d69
hex-nagivator <smheric01@gmail.com>
2025-01-23 01:28:13 +0800

    refactor: Simplify random site generation function

--- main.py
+++ main.py
@@ -1,46 +1,51 @@
 # Program for Valorant Competitive Parties
-
 import random
 
-#Date of Release
-maps = {"Bind", "Haven", "Split", "Ascent", "Icebox", "Breeze", "Fracture", "Pearl", "Lotus", "Sunset", "Abyss"}
-userSelected = False
-sites = 2
+#Collection of maps in lowercase
+maps = ["bind", "haven", "split", "ascent", "icebox", "breeze", "fracture", "pearl", "lotus", "sunset", "abyss"]
+#Collection of maps with first letter capitalize
+maps_capitalize = [word.capitalize() for word in maps]
 
-def translate_site(fSiteInt):
-    siteAlphabet = ""
-    if fSiteInt == 1:
-        siteAlphabet = "A"
-    elif  fSiteInt == 2:
-        siteAlphabet = "B"
-    elif fSiteInt == 3:
-        siteAlphabet = "C"
-    return siteAlphabet
+user_index = 0
 
+def introduction_prompt():
+    global user_index
+    while user_index == 0:
+        for i in maps_capitalize:
+            print(f"{i}", end = " ")
+        print()
+        user_input = input("Select A Map (Q to Quit): ").lower()
+        if user_input == "q":
+            break
+        elif user_input in maps:
+            user_index = 1
+            print(f"You've selected {user_input.capitalize()}.")
+            site_selection_prompt(user_input)
 
-def roll_attack_site(fMapChoice, fMapSites):
-    if fMapChoice == "haven": fMapSites = 3
-    print("Tough time deciding which site to attack? ")
-    rollOption = str(input(f"Roll for attack side for {fMapChoice.capitalize()} (Y or N):"))
-    if rollOption.lower() == "n":
-        print("Quit")
-    elif rollOption.lower() == "y":
-        randomSite = random.randint(1,fMapSites)
-        print(translate_site(randomSite))
-
-
-for i in maps:
-    print(f"{i}", end = " ")
-print()
-
-while not userSelected:
-    userOption = input("Select A Map (Q to Quit): ").lower()
-    if userOption == "q":
-        break
+def site_rolling(map_input):
+    if map_input == "haven":
+        no_of_sites = 3
     else:
-        for i in maps:
-            optionCheck = i.lower()
-            if userOption == optionCheck:
-                userSelected = True
-                print(f"You've selected {userOption.capitalize()}.")
-                roll_attack_site(userOption,sites)
+        no_of_sites = 2
+    random_site = random.randint(1, no_of_sites)
+    match random_site:
+        case 1:
+            print("You've rolled for A site.")
+        case 2:
+            print("You've rolled for B site.")
+        case 3:
+            print("You've rolled for C site.")
+
+def site_selection_prompt(map_input):
+    user_input = input("Would you like to roll for site to attack? (Y/N): ")
+    if user_input.lower() == "y":
+        site_rolling(map_input)
+    elif user_input.lower() == "n":
+        print()#bring them back to introduction prompt
+    else:
+        print("Invalid input.")
+
+def init():
+    introduction_prompt()
+
+init()
